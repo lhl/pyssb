376e49741bb31f2b995645436508d3f12d4929e3
lhl <lhl@randomfoo.net>
2015-09-18 04:18:13 -0700

    pyqt5 installation script

--- install-pyqt5.sh
+++ install-pyqt5.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# ARCH
-sudo pacman -S python-pyqt5 qt5-webkit
-
-# Ubuntu
-sudo apt-get install python-pyqt5.qtwebkit
-
-# MacPorts
-sudo port install py-pyqt5
-
-# Brew
-
+if [ "$(uname)" == "Darwin" ]; then
+  if [[ $(type -P "port") ]]; then
+    sudo port install py-pyqt5
+  elif [[ $(type -P "bre") ]]; then
+    sudo brew install pyqt5
+  fi
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  distro=`grep '^ID=' /etc/os-release | cut -d '=' -f 2`
+  if [ "$(distro)" == "arch" ] then;
+    sudo pacman -S python-pyqt5 qt5-webkit
+  elif [ "$(distro)" == "ubuntu" ] then;
+    sudo apt-get install python-pyqt5.qtwebkit
+  fi
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+  echo "Sorry, we don't support Windows yet. Submit a pull request?"
+fi
