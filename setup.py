2cbaadd525751d18561d69fd9909ef560b67c1e7
lhl <lhl@randomfoo.net>
2015-09-18 03:51:04 -0700

    basic setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, find_packages
+
+install_requires = [
+  "lassie>=0.6.0",
+]
+
+setup(
+  name = 'pyssb',
+  version = '0.1',
+  author = 'Leonard Lin',
+  author_email = 'lhl@randomfoo.net',
+  url = 'https://github.com/lhl/pyssb',
+  packages = find_packages(),
+  install_requires = install_requires,
+  description = 'PyQT5 Site Specific Browser',
+  download_url = 'https://github.com/lhl/pyssb/archive/master.zip',
+  keywords = ['browser', 'ssb'],
+  classifiers = [],
+)
